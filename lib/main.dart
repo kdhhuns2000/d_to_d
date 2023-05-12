0fb20a64c418099568860b99436f8fe9b8f7acbf
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2023-05-13 03:27:30 +0900

    Remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:d_to_d/screens/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'screens/login/login.dart';
 import 'package:d_to_d/utils/router.dart';
 
 void main() {

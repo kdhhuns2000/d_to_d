bfe8d97f3570bf82d997cb6fe72683275c43f129
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2023-05-16 19:18:49 +0900

    Modify directory

--- lib/screens/setting/setting_page.dart
+++ lib/screens/setting/setting_page.dart
@@ -1,3 +1,4 @@
+import 'package:d_to_d/screens/setting/setting_icons.dart';
 import 'package:flutter/material.dart';
 
 class SettingPage extends StatelessWidget {
@@ -5,8 +6,46 @@ class SettingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('hi'),
+    return Column(
+      children: [
+        ListTile(
+          title: Text("이한슬"),
+          subtitle: Text("대학생 디자이너"),
+          textColor: Colors.black,
+        ),
+        settingDivider(),
+        settingListTile("계정", SettingIcon.account),
+        settingDivider(),
+        settingListTile("알림설정", SettingIcon.bell),
+        settingDivider(),
+        settingListTile("깃허브", SettingIcon.github_inverted),
+        settingDivider(),
+        settingListTile("좋아요", SettingIcon.thumbsup),
+        settingDivider(),
+        settingListTile("내 게시글", SettingIcon.preview),
+        Expanded(
+          child: Image.asset('assets/images/img_setting.png',
+              width: double.infinity, fit: BoxFit.cover),
+        ),
+      ],
     );
   }
+}
+
+Widget settingListTile(String title, IconData icon) {
+  return ListTile(
+    horizontalTitleGap: 0,
+    title: Text(title),
+    leading: Icon(icon),
+    textColor: Colors.black,
+    iconColor: Colors.black,
+  );
+}
+
+Widget settingDivider() {
+  return Divider(
+    height: 0.8,
+    thickness: 0.8,
+    color: Color.fromARGB(255, 213, 213, 213),
+  );
 }

79e633198823467590e0470fb2585db9d24b7396
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2023-05-23 17:53:41 +0900

    Add login modal

--- lib/widgets/login/login_widget.dart
+++ lib/widgets/login/login_widget.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:d_to_d/utils/provider/login_state_provider.dart';
 
-final loginStateProvider = StateProvider<bool>((ref) => false);
+// final loginStateProvider = StateProvider<bool>((ref) => false);
 
 class LoginWidget extends ConsumerStatefulWidget {
   const LoginWidget({super.key});
@@ -12,52 +13,81 @@ class LoginWidget extends ConsumerStatefulWidget {
 }
 
 class _LoginWidgetState extends ConsumerState<LoginWidget> {
-  @override
-  void initState() {
-    super.initState();
-    final login = ref.read(loginStateProvider);
-    print(login);
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   final login = ref.read(loginStateProvider);
+  //   print(login);
+  // }
 
   @override
   Widget build(BuildContext context) {
     final loginValue = ref.watch(loginStateProvider);
+    final mediaWidth = MediaQuery.of(context).size.width;
+    final mediaHeight = MediaQuery.of(context).size.height;
+
     return Positioned(
-      bottom: MediaQuery.of(context).size.height * 0.15,
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.7,
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () => ref.read(loginStateProvider.notifier).state =
-                  !ref.read(loginStateProvider.notifier).state,
-              style: ElevatedButton.styleFrom(
-                  minimumSize: Size.fromHeight(50),
-                  padding: EdgeInsets.symmetric(horizontal: 16.0),
-                  backgroundColor: Colors.transparent,
-                  side: BorderSide(
-                    color: Colors.white,
-                    width: 2,
-                  ),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0))),
-              child: Text(
-                'Login$loginValue',
-                style: TextStyle(fontSize: 16.0),
+      bottom: mediaHeight * 0.15,
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            width: mediaWidth * 0.7,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(
+                width: 1.5,
+                color: Colors.white,
               ),
             ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                child: Text(
-                  'Sign in',
-                  style: TextStyle(color: Colors.white),
+            child: ExpansionTile(
+              title: Text(
+                'Login',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
                 ),
-                onPressed: () => {},
               ),
-            )
-          ],
-        ),
+              expandedAlignment: Alignment.center,
+              onExpansionChanged: (value) =>
+                  ref.watch(loginStateProvider.notifier).update(),
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: '아이디',
+                    hintStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    fillColor: Colors.white,
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: '비밀번호',
+                    hintStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () => _onLogin(),
+                  child: Text('로그인'),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: TextButton(
+              child: Text(
+                loginValue == true ? '' : 'Sign in',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () => {},
+            ),
+          )
+        ],
       ),
     );
   }

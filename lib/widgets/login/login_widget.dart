ddd7f47f3ace4ef8c74772767d37b30bbbb81169
Dohun <kdhhuns2000@gmail.com>
2023-05-23 11:40:09 +0900

    Add riverpod provider

--- lib/widgets/login/login_widget.dart
+++ lib/widgets/login/login_widget.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginWidget extends StatefulWidget {
-  const LoginWidget({
-    super.key,
-  });
+final loginStateProvider = StateProvider<bool>((ref) => false);
+
+class LoginWidget extends ConsumerStatefulWidget {
+  const LoginWidget({super.key});
 
   @override
-  State<LoginWidget> createState() => _LoginWidgetState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _LoginWidgetState();
 }
 
-class _LoginWidgetState extends State<LoginWidget> {
+class _LoginWidgetState extends ConsumerState<LoginWidget> {
+  @override
+  void initState() {
+    super.initState();
+    final login = ref.read(loginStateProvider);
+    print(login);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final loginValue = ref.watch(loginStateProvider);
     return Positioned(
-      top: MediaQuery.of(context).size.height * 0.3,
+      bottom: MediaQuery.of(context).size.height * 0.15,
       child: SizedBox(
         width: MediaQuery.of(context).size.width * 0.7,
         child: Column(
           children: [
             ElevatedButton(
-              onPressed: () => _onLogin(),
+              onPressed: () => ref.read(loginStateProvider.notifier).state =
+                  !ref.read(loginStateProvider.notifier).state,
               style: ElevatedButton.styleFrom(
                   minimumSize: Size.fromHeight(50),
                   padding: EdgeInsets.symmetric(horizontal: 16.0),
@@ -32,7 +42,7 @@ class _LoginWidgetState extends State<LoginWidget> {
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10.0))),
               child: Text(
-                'Login',
+                'Login$loginValue',
                 style: TextStyle(fontSize: 16.0),
               ),
             ),

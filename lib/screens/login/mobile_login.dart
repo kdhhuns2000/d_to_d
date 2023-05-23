76d90d7859e39a8682d1332daf66f1ac17333561
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2023-05-23 17:53:28 +0900

    Add image blur
    blur effect when login page open

--- lib/screens/login/mobile_login.dart
+++ lib/screens/login/mobile_login.dart
@@ -1,68 +1,88 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:d_to_d/widgets/login/login_widget.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:d_to_d/utils/provider/login_state_provider.dart';
 
-class MobileLogin extends StatelessWidget {
+class MobileLogin extends ConsumerWidget {
   const MobileLogin({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final loginValue = ref.watch(loginStateProvider);
     return Scaffold(
       body: Stack(
         alignment: Alignment.center,
         children: [
-          Column(
-            children: [
-              Flexible(
-                fit: FlexFit.tight,
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/bg_developer.jpg'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  width: double.infinity,
-                  alignment: Alignment.bottomLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'Developer',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 36.0,
-                        fontFamily: 'NotoSerifKR',
-                        fontWeight: FontWeight.w900,
+          Opacity(
+            opacity: loginValue ? 1.0 : 1.0,
+            child: Column(
+              children: [
+                Flexible(
+                  fit: FlexFit.tight,
+                  child: ImageFiltered(
+                    imageFilter: ImageFilter.blur(
+                        sigmaX: loginValue ? 5 : 0, sigmaY: loginValue ? 5 : 0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/bg_developer.jpg'),
+                          fit: BoxFit.cover,
+                        ),
                       ),
+                      width: double.infinity,
+                      alignment: Alignment.bottomLeft,
+                      child: loginValue == true
+                          ? null
+                          : Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                'Developer',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 36.0,
+                                  fontFamily: 'NotoSerifKR',
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                            ),
                     ),
                   ),
                 ),
-              ),
-              Flexible(
-                fit: FlexFit.tight,
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/bg_designer.jpg'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  width: double.infinity,
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      'Designer',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 36.0,
-                        fontFamily: 'NotoSerifKR',
-                        fontWeight: FontWeight.w900,
+                Flexible(
+                  fit: FlexFit.tight,
+                  child: ImageFiltered(
+                    imageFilter: ImageFilter.blur(
+                        sigmaX: loginValue ? 5 : 0, sigmaY: loginValue ? 5 : 0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/bg_designer.jpg'),
+                          fit: BoxFit.cover,
+                        ),
                       ),
+                      width: double.infinity,
+                      alignment: Alignment.topRight,
+                      child: loginValue == true
+                          ? null
+                          : Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                'Designer',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 36.0,
+                                  fontFamily: 'NotoSerifKR',
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                            ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
           LoginWidget(),
         ],
